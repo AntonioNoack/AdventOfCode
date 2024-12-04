9dfdf5ca6a2bfddadc03aa8652670e7f998e15b7
Antonio Noack <antonio-noack@gmx.de>
2024-12-04 07:35:58 +0100

    Solved day4

---
+++ src/me/anno/aoc24/day4/Part1.kt
@@ -0,0 +1,49 @@
+package me.anno.aoc24.day4
+
+import me.anno.aoc24.utils.Utils.readLines
+
+
+val searched = "XMAS"
+
+fun search(
+    lines: List<String>,
+    fx: Int, fy: Int,
+): Int {
+    val sx = lines[0].length
+    val sy = lines.size
+    var total = 0
+    for (xi in 0 until sx) {
+        field@ for (yi in 0 until sy) {
+            // check that last character is inside field, too
+            val ex = xi + fx * searched.lastIndex
+            val ey = yi + fy * searched.lastIndex
+            if (ex !in 0 until sx || ey !in 0 until sy) {
+                continue@field
+            }
+            // check that each letter is correct
+            for (ki in searched.indices) {
+                val cx = xi + fx * ki
+                val cy = yi + fy * ki
+                val letter = lines[cy][cx]
+                if (letter != searched[ki]) {
+                    continue@field
+                }
+            }
+            total++
+        }
+    }
+    return total
+}
+
+/**
+ * count XMAS in the riddle:
+ *  - reversed, horizontal, vertical, diagonal
+ * */
+fun main() {
+    val lines = readLines(4, "data.txt")
+    val sum = search(lines, +1, 0) + search(lines, -1, 0) +
+            search(lines, 0, +1) + search(lines, 0, -1) +
+            search(lines, +1, +1) + search(lines, -1, +1) +
+            search(lines, +1, -1) + search(lines, -1, -1)
+    println(sum)
+}
---
+++ src/me/anno/aoc24/day4/Part2.kt
@@ -0,0 +1,29 @@
+package me.anno.aoc24.day4
+
+import me.anno.aoc24.utils.Utils.readLines
+
+/**
+ * find all cross-MAS
+ * */
+fun main() {
+    val lines = readLines(4, "data.txt")
+    val sx = lines[0].length
+    val sy = lines.size
+    var sum = 0
+    for (yi in 0 until sy - 2) {
+        for (xi in 0 until sx - 2) {
+            // check that center is A
+            if (lines[yi + 1][xi + 1] != 'A') continue
+            // check that diagonals aren't identical
+            if (lines[yi][xi] == lines[yi + 2][xi + 2]) continue
+            if (lines[yi + 2][xi] == lines[yi][xi + 2]) continue
+            // check that all corners are M or S
+            if (lines[yi][xi] !in "MS") continue
+            if (lines[yi][xi + 2] !in "MS") continue
+            if (lines[yi + 2][xi] !in "MS") continue
+            if (lines[yi + 2][xi + 2] !in "MS") continue
+            sum++
+        }
+    }
+    println(sum)
+}
